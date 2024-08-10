c31cae43b1dd5090e65f63eeeffa4a533e8f25d4
Yash Nikam <yashdnikam14@gmail.com>
2024-08-10 14:40:38 +0530

    settings.py @5m

--- sky_an_app/settings.py
+++ sky_an_app/settings.py
@@ -129,3 +129,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
