5b1891ebfa103bee342de7e2dfd5a0bdeca9d506
unknown <yashdnikam14@gmail.com>
2024-09-28 19:11:01 +0530

    env settings

--- sky_an_app/env_settings/sky_env.py
+++ sky_an_app/env_settings/sky_env.py
@@ -2,9 +2,9 @@
 
 database_settings = {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sky_db',
-        'USER': 'root',
-        'PASSWORD': 'yash',
-        'HOST': 'localhost',
+        'NAME': 'yashnikam$sky_db',
+        'USER': 'yashnikam',
+        'PASSWORD': 'Yash@1234?',
+        'HOST': 'yashnikam.mysql.pythonanywhere-services.com',
         'PORT': '3306'
 }
